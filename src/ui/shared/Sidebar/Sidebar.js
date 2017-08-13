94d88f789f1925eefb98c655be0a8a44f8fc6ad7
Liu Lu <liuliu@lulu.com>
2017-08-13 20:22:11 +0800

    sidebar styling

--- src/ui/shared/Sidebar/Sidebar.js
+++ src/ui/shared/Sidebar/Sidebar.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+import './sidebar.css'
+import { slide as Menu } from 'react-burger-menu'
 
 class Sidebar extends Component {
   render(){
     return(
       <div className="sidebar">
-        Sidebar
+        <Menu isOpen>
+          <div className="bm-user-info">
+            <img src="http://media.haoduoshipin.com/yummy/default-avatar.png" alt="avatar" />
+            <div className="bm-user-auth">
+              <Link to="" className="bm-user-left">
+                lulu
+              </Link>
+              <Link to="" className="bm-user-right">
+                退出
+              </Link>
+            </div>
+          </div>
+        <div className="bm-link-list">
+        <Link to="/">Home</Link>
+        <Link to="/signup">注册</Link>
+        <Link to="/cart">购物车</Link>
+        <Link to="/dishes">猜你喜欢</Link>
+        </div>
+        <button className="bm-close-button">
+        关闭
+        </button>
+        </Menu>
       </div>
     )
   }

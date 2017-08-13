b51a22a09738816a7646719b6dcace3ec93a93a3
Liu Lu <liuliu@lulu.com>
2017-08-13 21:30:23 +0800

    signup form styling

--- src/ui/pages/Signup/Signup.js
+++ src/ui/pages/Signup/Signup.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import TitleHeader from '../../shared/TitleHeader/TitleHeader'
 import './signup.css'
+import { Link }from 'react-router-dom'
 
 class Signup extends Component {
   render(){
@@ -16,13 +17,21 @@ class Signup extends Component {
               连接小而确定的幸福
             </p>
           </div>
-          <div className='signup-form-wrap'>
-            <form className='signup-form'>
-
-            </form>
-          </div>
-          <div className='signup-actions'>
-            Actions
+          <form className='signup-form'>
+            <div className='signup-text-inputs'>
+              <div className='signup-text-inputs-inner'>
+                <input type='text' placeholder='用户名' />
+                <input type='text' placeholder='Email' />
+                <input type='password' placeholder='password' />
+                <input type='password' placeholder='再输一遍' />
+              </div>
+            </div>
+            <div className='signup-actions'>
+              <button type='submit'>注册</button>
+            </div>
+          </form>
+          <div className='signup-other-option'>
+            <Link to='/login'>已有账号？直接登录</Link>
           </div>
         </div>
       </div>

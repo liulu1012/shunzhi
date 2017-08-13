39ff45ebdbec10b1b6d2aed485564b2cd63f2341
Liu Lu <liuliu@lulu.com>
2017-08-13 19:33:41 +0800

    react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import Home from './ui/pages/Home/Home'
+import Signup from './ui/pages/Signup/Signup'
+import Sidebar from './ui/shared/Sidebar/Sidebar'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+}from 'react-router-dom'
 
 class App extends React.Component{
   render(){
     return(
       <div>
-        hello
+        <Router>
+          <div>
+            <Sidebar />
+            <Switch>
+              <Route exact path='/' component={ Home } />
+              <Route path='/signup' component={ Signup } />
+            </Switch>
+          </div>
+        </Router>
       </div>
     )
   }
---
+++ src/ui/shared/Sidebar/Sidebar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Sidebar extends Component {
+  render(){
+    return(
+      <div className="side-bar">
+        Sidebar
+      </div>
+    )
+  }
+}
+
+export default Sidebar

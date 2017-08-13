a25173014021e5b8061915575eea6767b7388199
Liu Lu <liuliu@lulu.com>
2017-08-13 21:45:03 +0800

    submit signup form

--- src/ui/pages/Signup/Signup.js
+++ src/ui/pages/Signup/Signup.js
@@ -1,37 +1,53 @@
 import React, { Component } from 'react'
 import TitleHeader from '../../shared/TitleHeader/TitleHeader'
 import './signup.css'
-import { Link }from 'react-router-dom'
+import axios from 'axios'
+import {
+  Link
+} from 'react-router-dom'
+import Settings from '../../../settings'
 
 class Signup extends Component {
-  render(){
+
+  signup = (e) => {
+    e.preventDefault()
+    let data = {
+      username: 'xxx',
+      password: '222'
+    }
+    axios.post(`${Settings.host}/user/signup`, data).then( res => {
+      console.log(res.data)
+    })
+  }
+
+  render() {
     return(
-      <div className="sign-up">
-        <TitleHeader title='signup' />
-        <div className='signup-content'>
-          <div className='signup-hero'>
-            <h1 className='title'>
+      <div className="signup">
+        <TitleHeader title="signup" />
+        <div className="signup-content">
+          <div className="signup-hero" >
+            <h1 className="title">
               注册
             </h1>
-            <p className='slogan'>
+            <p className="slogan">
               连接小而确定的幸福
             </p>
           </div>
-          <form className='signup-form'>
-            <div className='signup-text-inputs'>
-              <div className='signup-text-inputs-inner'>
-                <input type='text' placeholder='用户名' />
-                <input type='text' placeholder='Email' />
-                <input type='password' placeholder='password' />
-                <input type='password' placeholder='再输一遍' />
+          <form onSubmit={this.signup} className="signup-form">
+            <div className="signup-text-inputs">
+              <div className="signup-text-inputs-inner">
+                <input type="text" placeholder="用户名" />
+                <input type="text" placeholder="Email" />
+                <input type="password" placeholder="password" />
+                <input type="password" placeholder="再输一遍" />
               </div>
             </div>
-            <div className='signup-actions'>
-              <button type='submit'>注册</button>
+            <div className="signup-actions">
+              <button type="submit">注册</button>
             </div>
           </form>
-          <div className='signup-other-option'>
-            <Link to='/login'>已有账号？直接登录</Link>
+          <div className="signup-other-option">
+            <Link to="/login">已有账号？直接登录</Link>
           </div>
         </div>
       </div>
--- src/ui/shared/TitleHeader/TitleHeader.js
+++ src/ui/shared/TitleHeader/TitleHeader.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './title-header.css'
 
 const TitleHeader = ({ title }) => (
